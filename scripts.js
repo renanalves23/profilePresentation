75d997471fe935dd608e3e17fa1fc3f4ceca7b1f
renan <renanalves23@outlook.com>
2020-02-11 19:48:42 -0300

    Modal add active and modal remove active

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 const modalOverlay = document.querySelector(".modal-overlay")
 const cards = document.querySelectorAll('.card')
 
-for(let card of cards){
-
+for(let card of cards) {
+    card.addEventListener("click" , function() {
+      modalOverlay.classList.add("active")
+    })
 }
+
+const closeModal = document.querySelector('.close-modal')
+
+closeModal.addEventListener('click', function() {
+    modalOverlay.classList.remove('active')
+
+})
