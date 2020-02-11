22bdecfbf222f5f3b175c4a7698f1a63f3d407e7
renan <renanalves23@outlook.com>
2020-02-11 20:23:01 -0300

    Novos links colocados para os grids

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards) {
     card.addEventListener("click" , function() {
+      const videoId = 
       modalOverlay.classList.add("active");
       modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
