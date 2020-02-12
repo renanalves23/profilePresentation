e38fa38e2119c5596228b3b54b39b43f05a51ce1
renan <renanalves23@outlook.com>
2020-02-11 22:27:30 -0300

    nunjucks configurado para renderizar o index

--- Secao03/server.js
+++ Secao03/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
+server.set('view-engine', "html")
+
+nunjucks.configure('views', {
+  express:server
+})
+
 server.get('/', (req, res) => {
-    res.send({message: "Hello"})
+   return res.render('index')
 })
 
 server.listen(5000, () => {
