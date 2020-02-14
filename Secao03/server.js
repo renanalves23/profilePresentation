313ed558ba67d0a66ac6e6912fadcb01f74cbc45
renan <renanalves23@outlook.com>
2020-02-13 23:57:16 -0300

    Ultimo estado antes de mudar a rota do video para abrir em outra pagina utilizando o id

--- Secao03/server.js
+++ Secao03/server.js
@@ -13,7 +13,8 @@ server.set("view engine", "njk")
 
 nunjucks.configure("views", {
   express: server,
-  autoescape: false
+  autoescape: false,
+  noCache: true
 })
 
 server.get('/', (req, res) => {
