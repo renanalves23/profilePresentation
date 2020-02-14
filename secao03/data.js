3445588ccf50e3fd879bc51a2b2d1ab2a85e4f59
renan <renanalves23@outlook.com>
2020-02-13 21:26:28 -0300

    Criação do data.js

---
+++ secao03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "6Czd1Yetaac",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "63 min",
+    price: "Free"
+  },
+  {
+    id: "V3QGW2PgKKY",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "15 min",
+    price: "Free"
+  },
+  {
+    id: "lx_m3nfz5bI",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "15 min",
+    price: "Free"
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "54 min",
+    price: "Free"
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "34 min",
+    price: "Free"
+  },
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "58 min",
+    price: "Free"
+  }  
+]
